c723815dc6ec303a05a2c418a7ed4ae02f7cfb9e
surajpshenoy <surajpshenoy@gmail.com>
2021-07-21 12:45:15 +1000

    navigation buttons added

--- src/Review.js
+++ src/Review.js
@@ -16,6 +16,17 @@ return (
     <h4 className="author">{name}</h4>
     <p className="job">{job}</p> 
     <p className="info">{text}</p>
+    <div className="button-container">
+        <button className="prev-btn">
+            <FaChevronLeft />
+        </button>
+        <button className="next-btn">
+            <FaChevronRight />
+        </button>
+    </div>
+    <button className="random-btn">
+        random
+    </button>
 </article>
 
 
